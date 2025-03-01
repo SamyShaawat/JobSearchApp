7fe68b3d4be581189fd953ba2241772d461f6d52
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-03-01 10:11:08 +0200

    finish CRON Job for Deleting Expired OTP Codes

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import bootstrap from "./src/app.controller.js";
 import path from "path";
 
 dotenv.config({ path: path.resolve("config/.env") });
+import "./src/scheduler/schedule.js";
 
 const port = process.env.PORT || 3000;
 const app = express();
