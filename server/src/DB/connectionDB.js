fb50e60a71867f6d8ff74fec0005fec5591266ae
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-03-02 00:40:55 +0200

    convert connection of database to atlas

--- server/src/DB/connectionDB.js
+++ server/src/DB/connectionDB.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
 
 const connectionDB = async () => {
-  const uri = process.env.URI_CONNECTION || "mongodb://localhost:27017/JobSearchApp";
+  const uri = process.env.URI_CONNECTION ;
+  const DBName = process.env.DBName
   try {
     await mongoose.connect(uri);
-    console.log(`Connected to MongoDB at ${uri}`);
+    console.log(`Connected to MongoDB at ${DBName}`);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     process.exit(1); 
