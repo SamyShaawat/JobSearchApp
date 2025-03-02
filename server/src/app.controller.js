3f954ac428e422b41c32ffcd46ca8ce2fdc8c50e
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-03-02 04:15:56 +0200

    add graphql api and finish admin dashboard apis

--- server/src/app.controller.js
+++ server/src/app.controller.js
@@ -4,6 +4,7 @@ import chatRouter from "./modules/chats/chat.controller.js";
 import companyRouter from "./modules/companies/company.controller.js";
 import jobOpportunityRouter from "./modules/jobs/jobOpportunity.controller.js";
 import userRouter from "./modules/users/user.controller.js";
+import { graphqlMiddleware } from "../src/graphql/graphql.js";
 import { globalErrorHandler } from "./utils/errorHandling.js";
 import cors from "cors";
 
@@ -25,6 +26,7 @@ const bootstrap = async (app, express) => {
   app.use("/applications", applicationRouter);
   app.use("/chats", chatRouter);
 
+  app.use("/graphql", graphqlMiddleware);
   // connect to database and wait until it's successful
   await connectionDB();
 
